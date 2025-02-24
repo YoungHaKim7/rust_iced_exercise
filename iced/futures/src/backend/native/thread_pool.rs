ea5b0bb271c4c6c1a582a6a78f8b29eb4f055e46
GYoung <ytok1108@aol.com>
2025-02-24 19:54:30 +0900

    iced (Rust) 0.13.1

---
+++ iced/futures/src/backend/native/thread_pool.rs
@@ -0,0 +1,18 @@
+//! A `ThreadPool` backend.
+
+/// A thread pool executor for futures.
+pub type Executor = futures::executor::ThreadPool;
+
+impl crate::Executor for Executor {
+    fn new() -> Result<Self, futures::io::Error> {
+        futures::executor::ThreadPool::new()
+    }
+
+    fn spawn(&self, future: impl Future<Output = ()> + Send + 'static) {
+        self.spawn_ok(future);
+    }
+}
+
+pub mod time {
+    //! Listen and react to time.
+}
---
+++ iced/futures/src/backend/null.rs
@@ -0,0 +1,21 @@
+//! A backend that does nothing!
+
+/// An executor that drops all the futures, instead of spawning them.
+#[derive(Debug)]
+pub struct Executor;
+
+impl crate::Executor for Executor {
+    fn new() -> Result<Self, futures::io::Error> {
+        Ok(Self)
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    fn spawn(&self, _future: impl Future<Output = ()> + Send + 'static) {}
+
+    #[cfg(target_arch = "wasm32")]
+    fn spawn(&self, _future: impl Future<Output = ()> + 'static) {}
+}
+
+pub mod time {
+    //! Listen and react to time.
+}
